92a485ca2042c610c1f542e4c00b6cffb44b03f4
mtbr39 <nm1552jp@gmail.com>
2021-05-27 02:45:56 +0900

    saveFeel()

--- firebase_controll/diary_prototype/diary_vue.js
+++ firebase_controll/diary_prototype/diary_vue.js
@@ -4,7 +4,7 @@ const app = {
     return {
       email: "",
       password: "",
-      user_email: "",
+      user: "",
       date: {},
       hours_text: "",
       minutes_text: "",
@@ -33,7 +33,7 @@ const app = {
       fs.signUp({ email: this.email, password: this.password });
     },
     updateTime() {
-      let date = new Date()
+      let date = new Date();
 
       this.hours_text = date.getHours();
       this.minutes_text = date.getMinutes();
@@ -42,7 +42,24 @@ const app = {
       this.date_text = date.getFullYear() + '年'
         + month + '月'
         + date.getDate() + '日';
+      
+      this.date = date.getTime();
     },
+    saveFeel() {
+      db.collection("users")
+        .doc(this.user.uid)
+        .collection("feelings")
+        .add({
+          type: 3,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        .then(function () {
+          console.log("きもち保存!");
+        })
+        .catch(function (error) {
+          console.error("Error writing document: ", error);
+        });
+    }
   },
 };
 
@@ -50,5 +67,15 @@ const vm = Vue.createApp(app).mount("#app");
 
 firebase.auth().onAuthStateChanged((user) => {
   console.log("現在のユーザー2", user.email);
-  vm.user_email = user.email;
+  vm.user = user;
 });
+
+// db.collection("users")
+//   .where("id", "==", "現在のユーザーのID") //現在のユーザーを取り出す
+//   .onSnapshot(function (querySnapshot) {
+//     var cities = [];
+//     querySnapshot.forEach(function (doc) {
+//       cities.push(doc.data().name);
+//     });
+//     console.log("Current cities in CA: ", cities.join(", "));
+//   });
