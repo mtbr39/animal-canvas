48fd396ff7e99f81b1c9d2d3e72d85988fe35618
mtbr39 <nm1552jp@gmail.com>
2023-07-27 01:10:55 +0900

    add: frame loop

--- canvas/path/app.js
+++ canvas/path/app.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener('load', () => {
 
     let canvas = document.getElementById('canvas');
@@ -10,25 +8,36 @@ window.addEventListener('load', () => {
     let cw = canvas.width / 250;
     let org = {x: 0, y: 0};
 
-    let ctx2 = new Ctx2({ctx: ctx, cw: cw, org: org});
-
-    let points = {
-        A: {x: 20, y: 80},
-        B: {x: 0, y: 0}
-    };
-
     //クラスインスタンス生成など
+    const drawer = new Drawer({canvas: canvas, ctx: ctx, cw: cw, org: org});
+    
     let alphaAnimals = [];
     alphaAnimals[1] = new Animal();
+    
+    drawer.submitObject(alphaAnimals[1]);
 
-    draw();
+    // FrameLoop
+    const fps = 60;
 
-    //描写
-    function draw() {
-        ctx2.drawLine(points.A, points.B);
+    let targetInterval = 1000 / fps;
+    let previousTime = Date.now() - targetInterval;
 
-        
+    function loop() {
+        let currentTime = Date.now();
+        if (currentTime - previousTime > targetInterval) {
+            update();
+            previousTime = Date.now();
+        }
+        requestAnimationFrame(loop);
     }
+
+    function update() {
+        drawer.update();
+        drawer.draw();
+
+    }
+
+    loop();
 
     // ---- マウスイベント ----
 
@@ -41,9 +50,6 @@ window.addEventListener('load', () => {
     });
 
     canvas.addEventListener('mousemove', (e) => {
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw();
         
     });
 
@@ -53,104 +59,6 @@ window.addEventListener('load', () => {
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-
-        ctx2.cw = canvas.width / 300;
-
-        draw();
     };
 
 })
-
-
-class Ctx2 {
-    constructor(option) {
-        this.ctx = option.ctx;
-        this.cw = option.cw;
-        this.org = option.org;
-    }
-    vertexTransformPoint(pointRotate, pointCenter, pointRotate2, pointCenter2, pointControl, rotateProps) {
-        rotateProps.angleMouse = utl.angleLine(pointCenter, pointControl);
-        rotateProps.angleDiffMouse = rotateProps.angleMouse - rotateProps.angleMouseOnClick;
-        rotateProps.angleMouseOnClick = rotateProps.angleMouse;
-        return {point1: utl.rotatePoint(pointRotate, rotateProps.angleDiffMouse, pointCenter), point2: utl.rotatePoint(pointRotate2, rotateProps.angleDiffMouse, pointCenter2)};
-    }
-    drawLine(p1, p2, option = {}) {
-        this.ctx.beginPath();
-        this.modMoveTo(p1);
-        this.modLineTo(p2);
-        let lineColor = 'black'; 
-        if (option.edgeName && option.edgeName == option.clickTarget) {
-            lineColor = 'red';
-        }
-        this.callStyle({strokeStyle: lineColor});
-        this.ctx.stroke();
-    }
-    modMoveTo(p) {
-        this.ctx.moveTo( this.canvasPoint(p).x, this.canvasPoint(p).y );
-    }
-    modLineTo(p) {
-        this.ctx.lineTo( this.canvasPoint(p).x, this.canvasPoint(p).y );
-    }
-    callStyle(option = {}) {
-        this.ctx.strokeStyle = option.strokeStyle ?? 'black';
-        this.ctx.lineWidth = option.lineWidth ?? 2;
-        this.ctx.globalAlpha = 1.0;
-        this.ctx.lineCap = 'round';
-        this.ctx.lineJoin = 'round';
-    }
-    inverseMod(p) {
-        p = {x: p.x / this.cw - this.org.x, y: p.y / this.cw - this.org.y};
-        return p;
-    }
-    modFillText(text, point, offset={x:0, y:0}) {
-        this.ctx.font = `${8*this.cw}px serif`;
-        this.ctx.fillStyle = "black";
-        this.ctx.fillText(text, this.canvasPoint(point).x - offset.x*this.cw, this.canvasPoint(point).y - offset.y*this.cw);
-    }
-    modFillText2(text, point, angle, offset={x:10, y:10}) {
-        this.ctx.font = `${6*this.cw}px serif`;
-        let pointDisplay = {x: point.x + offset.x, y: point.y + offset.y};
-        pointDisplay = utl.rotatePoint(pointDisplay, angle, point);
-        this.ctx.fillStyle = "black";
-        this.ctx.fillText(text, this.canvasPoint(pointDisplay).x - 4*this.cw, this.canvasPoint(pointDisplay).y + 2*this.cw);
-    }
-    canvasPoint(point) {
-        return {x: (this.org.x + point.x) * this.cw, y: (this.org.y + point.y) * this.cw}
-    }
-    modArc(p) {
-        this.ctx.beginPath();
-        this.modMoveTo(p);
-        this.ctx.arc(this.canvasPoint(p).x, this.canvasPoint(p).y, 100, 0 * Math.PI / 180, 45 * Math.PI / 180, false);
-        this.ctx.fillStyle = 'black';
-        this.ctx.fill();
-    }
-    circle(p, radius) {
-        this.ctx.beginPath();
-        this.ctx.arc(this.canvasPoint(p).x, this.canvasPoint(p).y, radius*0.2*this.cw, 0, Math.PI * 2, true);
-        this.ctx.fillStyle = "#86efac";
-        this.ctx.fill();
-    }
-    drawCornerArc(point, p2, p3, angle) {
-        this.ctx.beginPath();
-        this.ctx.arc(this.canvasPoint(point).x, this.canvasPoint(point).y, 10*this.cw, 0, angle, false);
-        this.ctx.fillStyle = "#86efac";
-        this.ctx.fill();
-        this.ctx.beginPath();
-        this.modMoveTo(point);
-        this.modLineTo( utl.pointOnLineByDistance(point, p2, 10) );
-        this.modLineTo( utl.pointOnLineByDistance(point, p3, 10) );
-        this.ctx.fillStyle = "#86efac";
-        this.ctx.fill();
-    }
-    drawCornerRect(point, p2, p3) {
-        let pointStart = utl.pointOnLineByDistance(point, p2, 6);
-        let pointEnd   = utl.pointOnLineByDistance(point, p3, 6);
-        let diff = {x: pointStart.x - point.x, y: pointStart.y - point.y}
-        this.ctx.beginPath();
-        this.modMoveTo( pointStart );
-        this.modLineTo( {x: pointEnd.x + diff.x, y: pointEnd.y + diff.y} );
-        this.modLineTo( pointEnd );
-        this.ctx.strokeStyle = "#ef4444";
-        this.ctx.stroke();
-    }
-}
--- canvas/path/chainsystem.js
+++ canvas/path/chainsystem.js
@@ -2,8 +2,7 @@
 
 class Animal {
     constructor() {
-        this.x = 0;
-        this.y = 0;
+        this.position = {x:0, y:0}
         this.direction = 0;
         this.width = 0;
         this.height = 0;
@@ -11,14 +10,120 @@ class Animal {
     }
 
     update() {
-        this.x += 1;
+        this.position.x += 1;
     }
 
 
 }
 
 class Drawer {
-    draw(x, y) {
+    constructor(option) {
+        this.canvas = option.canvas;
+        this.ctx = option.ctx;
+        this.cw = option.cw;
+        this.org = option.org;
+        this.objects = [];
+    }
 
+    submitObject(object) {
+        this.objects.push(object);
+    }
+    update() {
+        this.objects.forEach( (object) => {
+            object.update();
+        } );
+    }
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.objects.forEach( (object) => {
+            this.drawObject(object);
+        } );
+    }
+    drawObject(object) {
+        if (object.drawType === undefined) {
+            this.circle(object.position, 100);
+        }
+    }
+
+    
+    canvasPoint(point) {
+        return {x: (this.org.x + point.x) * this.cw, y: (this.org.y + point.y) * this.cw}
+    }
+    drawLine(p1, p2, option = {}) {
+        this.ctx.beginPath();
+        this.modMoveTo(p1);
+        this.modLineTo(p2);
+        let lineColor = 'black'; 
+        if (option.edgeName && option.edgeName == option.clickTarget) {
+            lineColor = 'red';
+        }
+        this.callStyle({strokeStyle: lineColor});
+        this.ctx.stroke();
+    }
+    modMoveTo(p) {
+        this.ctx.moveTo( this.canvasPoint(p).x, this.canvasPoint(p).y );
+    }
+    modLineTo(p) {
+        this.ctx.lineTo( this.canvasPoint(p).x, this.canvasPoint(p).y );
+    }
+    style(option = {}) {
+        this.ctx.strokeStyle = option.strokeStyle ?? 'black';
+        this.ctx.lineWidth = option.lineWidth ?? 2;
+        this.ctx.globalAlpha = 1.0;
+        this.ctx.lineCap = 'round';
+        this.ctx.lineJoin = 'round';
+    }
+    inverseMod(p) {
+        p = {x: p.x / this.cw - this.org.x, y: p.y / this.cw - this.org.y};
+        return p;
+    }
+    modFillText(text, point, offset={x:0, y:0}) {
+        this.ctx.font = `${8*this.cw}px serif`;
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText(text, this.canvasPoint(point).x - offset.x*this.cw, this.canvasPoint(point).y - offset.y*this.cw);
+    }
+    modFillText2(text, point, angle, offset={x:10, y:10}) {
+        this.ctx.font = `${6*this.cw}px serif`;
+        let pointDisplay = {x: point.x + offset.x, y: point.y + offset.y};
+        pointDisplay = utl.rotatePoint(pointDisplay, angle, point);
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText(text, this.canvasPoint(pointDisplay).x - 4*this.cw, this.canvasPoint(pointDisplay).y + 2*this.cw);
+    }
+    modArc(p) {
+        this.ctx.beginPath();
+        this.modMoveTo(p);
+        this.ctx.arc(this.canvasPoint(p).x, this.canvasPoint(p).y, 100, 0 * Math.PI / 180, 45 * Math.PI / 180, false);
+        this.ctx.fillStyle = 'black';
+        this.ctx.fill();
+    }
+    circle(p, radius) {
+        this.ctx.beginPath();
+        this.ctx.arc(this.canvasPoint(p).x, this.canvasPoint(p).y, radius*0.2*this.cw, 0, Math.PI * 2, true);
+        this.ctx.fillStyle = "#86efac";
+        this.ctx.fill();
+    }
+    drawCornerArc(point, p2, p3, angle) {
+        this.ctx.beginPath();
+        this.ctx.arc(this.canvasPoint(point).x, this.canvasPoint(point).y, 10*this.cw, 0, angle, false);
+        this.ctx.fillStyle = "#86efac";
+        this.ctx.fill();
+        this.ctx.beginPath();
+        this.modMoveTo(point);
+        this.modLineTo( utl.pointOnLineByDistance(point, p2, 10) );
+        this.modLineTo( utl.pointOnLineByDistance(point, p3, 10) );
+        this.ctx.fillStyle = "#86efac";
+        this.ctx.fill();
+    }
+    drawCornerRect(point, p2, p3) {
+        let pointStart = utl.pointOnLineByDistance(point, p2, 6);
+        let pointEnd   = utl.pointOnLineByDistance(point, p3, 6);
+        let diff = {x: pointStart.x - point.x, y: pointStart.y - point.y}
+        this.ctx.beginPath();
+        this.modMoveTo( pointStart );
+        this.modLineTo( {x: pointEnd.x + diff.x, y: pointEnd.y + diff.y} );
+        this.modLineTo( pointEnd );
+        this.ctx.strokeStyle = "#ef4444";
+        this.ctx.stroke();
     }
 }
